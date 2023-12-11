a748c56473e1c4fd188a3a7608c8ece8b4a9c133
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-11 12:07:28 +0100

    Finished Get area Square Method. Closes #2

--- ACT23GetArea/Program.cs
+++ ACT23GetArea/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ACT23
+{
+    public class ACT23
+    {
+        public static double GetAreaSquare(double Length)
+        {
+            return Length * Length;
+        }
+    }
+}
