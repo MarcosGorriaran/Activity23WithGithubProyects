39bcd87468a932b4343ba8095a2caf7a3ca8dc0e
MarcosGorriaran <Marcos.Gorriaran.7e7@itb.cat>
2023-12-13 12:10:06 +0100

    Added expected messeges as constants

--- ACT23GetArea/ACT23GetArea.cs
+++ ACT23GetArea/ACT23GetArea.cs
@@ -6,6 +6,15 @@ namespace ACT23
     {
         public static void Main()
         {
+            const string WelcomeMsg = "Welcome to this program which will calculate the area of a square, a rectangle, a circle and a pentagon: ";
+            const string AskSquareLength = "Provide me with the length of the square: ";
+            const string AskRectangleLengths = "Provide me with the length of both sides of the rectangle splited with a blank space: ";
+            const string AskCircleRadius = "Provide me with the radius of the circle: ";
+            const string AskPentagonApothem = "Provide me with the apothem of the pentagon: ";
+            const string AskPentagonLength = "Provide me with the Length of the pentagon: ";
+
+            string length;
+
             
         }
         public static double GetAreaSquare(double length)
